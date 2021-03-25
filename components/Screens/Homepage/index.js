6ed868848327dfe2f2fc6de22b11ddec64b4a4de
patradanai <bloody_jojo@hotmail.com>
2021-03-25 11:29:42 +0700

    create stucture folder, box comp

---
+++ components/Screens/Homepage/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Box from "../../elements/Box";
+const HomePage = () => {
+  return (
+    <div className="container">
+      <div className="w-80 h-80">
+        <div className="grid grid-cols-3 grid-flow-row h-full w-full">
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ components/elements/Box/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Box = () => {
+  return <div className="bg-yellow-200 border-2 w-full h-full">1</div>;
+};
+
+export default Box;
